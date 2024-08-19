46008a3148d2c75197c6904c4c9b7cb884905bd8
htx <htx@pc.com>
2024-08-19 17:11:07 +0800

    feat:update

--- scripts/download_bin.sh
+++ scripts/download_bin.sh
@@ -9,10 +9,10 @@ log_info() {
 }
 
 PKG_LISTS=(
-    'https://objects.githubusercontent.com/github-production-release-asset-2e65be/134017286/021d9fa2-84ac-4f74-82f6-df8022d53bfb?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20240819%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240819T085331Z&X-Amz-Expires=300&X-Amz-Signature=619543a81cd4db582ffc80b11181ca914fb8307d2fc3898ce9a5b4d8d3862339&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=134017286&response-content-disposition=attachment%3B%20filename%3Dlazygit_0.43.1_Linux_x86_64.tar.gz&response-content-type=application%2Foctet-stream'
+    'https://objects.githubusercontent.com/github-production-release-asset-2e65be/23357588/fd60ddfc-1c16-455d-a15f-4a2abb2c12f9?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20240819%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20240819T090912Z&X-Amz-Expires=300&X-Amz-Signature=3d9c2235375087cd8401c72b32f7bed9d2b55902c153ac6e9ba91523552abfbf&X-Amz-SignedHeaders=host&actor_id=0&key_id=0&repo_id=23357588&response-content-disposition=attachment%3B%20filename%3Dprotoc-27.3-linux-x86_64.zip&response-content-type=application%2Foctet-stream'
 )
 
-mkdir pkgs
+mkdir -p pkgs
 cd pkgs
 
 # 下载软件包
